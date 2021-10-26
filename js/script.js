2c43701d7f0236099b394ce5f004cde9cd8aabc0
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-26 15:40:31 +0200

    add icon in card container with js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,134 @@
 "use strict"
+
+// Creo array di oggetti contenente le icone
+
+const iconArray = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+]
+
+// Individuo nell'html il contenitore dove inserire le card
+
+const cardContainer = document.getElementById("cardContainer")
+
+// Tramite ciclo for inserisco le icone nell'html
+for (let i = 0; i < iconArray.length; i++) {
+    const singleIcon = iconArray[i];
+
+    const { name, prefix, type, family } = singleIcon
+
+    let iconColor;
+
+    switch (type) {
+        case "animal":
+            iconColor = "c_icon_blue"
+            break;
+
+        case "vegetable":
+            iconColor = "c_icon_yellow"
+            break;
+
+        case "user":
+            iconColor = "c_icon_purple"
+            break;
+    }
+
+    cardContainer.innerHTML += `<li>
+                                    <i class="${family} ${prefix}${name} ${iconColor}"></i>
+                                    <h4>${name}</h4>
+                                </li>`
+}
